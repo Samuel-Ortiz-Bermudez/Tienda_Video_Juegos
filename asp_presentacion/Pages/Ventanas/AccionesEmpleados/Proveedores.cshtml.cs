d4dd53ce53a4cd27f9c3db367f830063abc69605
LEGN031 <medinae663@gmail.com>
2025-05-26 19:11:29 -0500

    Se añadieron pages suministros y proveedores

---
+++ asp_presentacion/Pages/Ventanas/AccionesEmpleados/Proveedores.cshtml.cs
@@ -0,0 +1,99 @@
+using lib_dominio.Entidades;
+using lib_dominio.Nucleo;
+using lib_presentaciones.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_presentacion.Pages.Ventanas.Perfiles
+{
+    [Authorize(Roles = "Admin,Empleado")]
+
+    public class ProveedoresModel : PageModel
+    {
+        private IProveedoresPresentacion? iPresentacionProveedor = null;
+
+        public ProveedoresModel(IProveedoresPresentacion? iPresentacionProveedor)
+        {
+            this.iPresentacionProveedor = iPresentacionProveedor;
+            Proveedor = new Proveedores();
+        }
+
+        [BindProperty] public Enumerables.Ventanas Accion { get; set; }
+        [BindProperty] public Proveedores? Proveedor { get; set; }
+        [BindProperty] public List<Proveedores>? Lista { get; set; }
+
+        public void OnGet()
+        {
+            OnPostIngreso();
+        }
+
+        public void OnPostIngreso()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Listas;
+
+                var proveedoresTask = iPresentacionProveedor!.Listar();
+                proveedoresTask.Wait();
+                Lista = proveedoresTask.Result;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtnEditar(int id)
+        {
+            try
+            {
+                OnPostIngreso();
+                Accion = Enumerables.Ventanas.Editar;
+                Proveedor = Lista!.FirstOrDefault(p => p.Id == id);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtnGuardar()
+        {
+            try
+            {
+                var guardarProveedor = iPresentacionProveedor!.Modificar(Proveedor);
+                guardarProveedor.Wait();
+                Proveedor = guardarProveedor.Result;
+
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostIngreso();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtnCerrar()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostIngreso();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public async Task<IActionResult> OnPostBtnCerrarSesion()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/Ventanas/Videojuegos");
+        }
+    }
+}
